acb287ff5a33aca326a832f5b5a554d49b165d97
Thiago Augusto <thiagoeu@gmail.com>
2018-04-12 15:05:59 -0300

    fix evalFile for my use case

--- renderer/tasks/patch.js
+++ renderer/tasks/patch.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const path      = require('path');
-const fs        = require('fs-extra');
-const async     = require('async');
+const path = require('path');
+const fs = require('fs-extra');
+const async = require('async');
 
 function getAllExpressions(data) {
-    return data.match(/\<expr bdata=\"([a-f0-9]+)\"\s*\/\>/gi);
+    let regex = /evalFile\(\"([\S\s]*?)\"\)/gi;
+    return regex.exec(data);
 }
 
 /**
@@ -15,17 +16,20 @@ function getAllExpressions(data) {
  *     "/Users/Name/Projects/MyProject/"
  *     "C:\\Projects\\MyNewProject\\"
  *     "/usr/var/tmp/projects/123/"
- * 
+ *
  * And will replace them to string `dst`
  */
 function replacePath(src, dst) {
-    return src.replace( /(?:(?:[A-Z]\:|~){0,1}(?:\/|\\\\|\\)(?=[^\s\/]))(?:(?:[\ a-zA-Z0-9\+\-\_\.\$\●\-]+(?:\/|\\\\|\\)))*/gm, dst);
+    return src.replace(
+        /(?:(?:[A-Z]\:|~){0,1}(?:\/|\\\\|\\)(?=[^\s\/]))(?:(?:[\ a-zA-Z0-9\+\-\_\.\$\●\-]+(?:\/|\\\\|\\)))*/gm,
+        dst
+    );
 }
 
 function processTemplateFile(project, callback) {
     // project file template name
-    let projectName     = path.join( project.workpath, project.template );
-    let replaceToPath   = path.join( process.cwd(), project.workpath, path.sep); // absolute path
+    let projectName = path.join(project.workpath, project.template);
+    let replaceToPath = path.join(process.cwd(), project.workpath, path.sep); // absolute path
 
     // escape single backslash to double in win
     replaceToPath = replaceToPath.replace(/\\/g, '\\\\');
@@ -43,25 +47,21 @@ function processTemplateFile(project, callback) {
         // search for expressions
         let expressions = getAllExpressions(data);
 
+        delete expressions[0];
         // check for existing expressions
         if (expressions !== null) {
-
             // then iterate over them
             for (let expr of expressions) {
+                if(!expr) continue;
 
-                // extract hex from xml tag and decode it
-                let hex = expr.split('"')[1];
-                let dec = new Buffer(hex, 'hex').toString('utf8');
-    
-                // do patch and encode back to hex
-                // using regex file path pattern
-                let enc = new Buffer( replacePath( dec, replaceToPath ) ).toString('hex');
-    
+                // replace old path with the new one
+                let newExpr = replacePath(expr, replaceToPath);
+
                 // replace patched hex
-                data = data.replace( hex, enc );
+                data = data.replace(expr, newExpr);
             }
         }
-        
+
         // save result
         fs.writeFile(projectName, data, callback);
     });
@@ -74,16 +74,15 @@ function processTemplateFile(project, callback) {
  */
 module.exports = function(project) {
     return new Promise((resolve, reject) => {
-
         console.info(`[${project.uid}] patching project...`);
 
-        // Iterate over assets, 
-        // skip those that are not data/script files, 
+        // Iterate over assets,
+        // skip those that are not data/script files,
         for (let asset of project.assets) {
             if (['script', 'data'].indexOf(asset.type) === -1) continue;
 
-            return processTemplateFile(project, (err) => {
-                return (err) ? reject(err) : resolve(project);
+            return processTemplateFile(project, err => {
+                return err ? reject(err) : resolve(project);
             });
         }
 
